551c513df5fd2d43c518b881000deb1dedc0170e
Khoi Bui <khoi@spiffy.ai>
2024-11-29 17:54:46 -0800

    set some project level settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Home } from '@pages/Home';
+import styled from 'styled-components';
 import Header from './Header';
 import { Footer } from './Footer';
-import { Body } from './Body';
-import styled from 'styled-components';
-// import React from 'react';
-import {Outlet, createBrowserRouter,RouterProvider } from "react-router-dom"
-
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,44 +11,22 @@ const Wrapper = styled.div`
   background: white;
   color: black;
   width: 100vw;
-  height: 100vh;
-
+  overflow: scroll;
 `;
-const routes = createBrowserRouter([
-  {path: "/", 
-    element: (
-      <Wrapper>
-        <Header />
-        <Body />
-        <Footer />
-      </Wrapper>
-    ),
-  children: [
-     
-    {path: "/", element: <div></div>}, 
-    {path:"/our-story",element:<div></div>},
-    {path:"/our-mission", element: <div></div>},
-    {path:"/our-board-members",element:<div></div>},
-    {path: "/projects", element: <div></div>},
-    {path:"/past-medical-missions",element:<div></div>},
-    {path: "/get-involved", element: <div></div>},
-    {path:"/copy-of-apply",element:<div></div>},
-    {path:"/registration-process", element: <div></div>},
-    {path:"/logistics",element:<div></div>},
-    {path: "/donate", element: <div></div>}, 
-    {path:"/application-payment",element:<div></div>},
-    {path: "/blog", element: <div></div>},
-    {path: "/faq", element: <div></div>},
-    {path: "/contact", element: <div></div>},
-    ]
-  }
-]);
-
 
 function App() {
   return (
-    <RouterProvider router = {routes} />
-
+    <Router>
+      <Wrapper>
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Wrapper>
+    </Router>
   );
 }
 
