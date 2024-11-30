504e646aa1c0d66ebc3e25d4820711ab63a3e9af
Khoi Bui <khoi@spiffy.ai>
2024-11-29 18:37:40 -0800

    donate page and form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from '@pages/Home';
 import styled from 'styled-components';
 import Header from './Header';
 import { Footer } from './Footer';
+import { DonateForm } from '@common/forms/DonateForm';
 
 const Wrapper = styled.div`
   display: flex;
@@ -22,6 +23,7 @@ function App() {
         <main>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/donate' element={<DonateForm />} />
           </Routes>
         </main>
         <Footer />
